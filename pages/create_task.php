34c8ec592082571e72b30876dbe87f03aa291404
Bikalpa Pahari <bp359@afsconnect1.njit.edu>
2017-12-17 21:42:19 -0500

    Added timestamp to create date

---
+++ pages/create_task.php
@@ -0,0 +1,48 @@
+<?php include 'header.php' ?>
+
+
+<article>
+
+
+<form action="https://web.njit.edu/~bp359/mvc/index.php?page=tasks&action=store" method="POST" name="form">
+
+    <?php date_default_timezone_set('America/New_York'); ?>
+
+
+    <div class="container">
+
+        <h1>Create Task</></h1>
+
+        <br>
+
+        <label><b>createddate</b></label>
+        <input type="text"name="createddate" value=<?php echo date('Y-m-d')?> readonly >
+
+        <br>
+
+        <label><b>duedate</b></label>
+        <input type="text" value="" name="duedate" >
+        
+
+        <br>
+
+        <label><b>message</b></label>
+
+        <textarea name="message" cols="50" rows="5"> </textarea>
+
+        <br>
+
+        <label><b>isdone</b></label>
+        <input type="text" value="" name="isdone" >
+
+        <br>
+
+        <input type="submit" name="btSubmit" value="Add Task">
+
+
+
+</form>
+
+    </article>
+
+<?php include 'footer.php' ?>
